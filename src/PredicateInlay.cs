dff00422b67af31b5c9ed3af46aac7dded43746d
thalber <73020111+thalber@users.noreply.github.com>
2022-10-12 22:39:14 +0400

    public tokenize

--- src/PredicateInlay.cs
+++ src/PredicateInlay.cs
@@ -64,7 +64,7 @@ public sealed partial class PredicateInlay
     /// </summary>
     /// <param name="expression"></param>
     /// <returns>An array of tokens.</returns>
-    private List<Token> Tokenize(string expression)
+    public List<Token> Tokenize(string expression)
     {
         List<Token> tokens = new();
         string remaining = expression.Clone() as string;
