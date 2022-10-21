e542f980b6b8e26ccc17ec0e3fa7a9d5b865e7ad
thalber <73020111+thalber@users.noreply.github.com>
2022-10-21 20:32:31 +0400

    Update PredicateInlay.cs

--- src/PredicateInlay.cs
+++ src/PredicateInlay.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using System.Reflection.Emit;
+#pragma warning disable CS1591
 /// <summary>
 /// Parses a string by logical expression operators (symbolic as well as words and/or/xor/not) into a treelike structure, then, using a supplied <see cref="del_FetchPred"/>, exchanges resulting substrings between logical operators into delegates, then evaluates expression on demand.
 /// </summary>
@@ -475,3 +476,4 @@ public sealed partial class PredicateInlay
     }
     #endregion
 }
+#pragma warning restore CS1591
