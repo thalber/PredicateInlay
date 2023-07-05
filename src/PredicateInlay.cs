160441bb17b3593b67e6dba2335da9bfbbb0792c
thalber <filefillman@gmail.com>
2023-07-06 01:29:39 +0400

    fix a regex to allow words with numbers

--- src/PredicateInlay.cs
+++ src/PredicateInlay.cs
@@ -592,7 +592,7 @@ public sealed partial class PredicateInlay
 			TokenType.Separator => new Regex("[\\s,]+", regexops),
 			TokenType.Operator => new Regex("!=|[&|^!]|(and|or|xor|not)(?=\\s)", regexops),
 			TokenType.Literal => new Regex("(?<=')[^']*(?=')|-?\\d+(\\.\\d+)?", regexops),
-			TokenType.Word => new Regex("[a-zA-Z_]+", regexops),
+			TokenType.Word => new Regex("[\\w]+", regexops),
 			//TokenType.Discard => throw new NotImplementedException(),
 			_ => throw new IndexOutOfRangeException("Supplied invalid token type"),
 		};
